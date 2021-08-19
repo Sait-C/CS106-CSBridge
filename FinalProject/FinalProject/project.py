fca9691639fe52da552c8c777e664cc19f9cc0a8
Muharrem Sait ÇOKTAŞ <coktasmuharremsait@gmail.com>
2021-08-19 13:23:53 +0300

    Update Final Project

--- FinalProject/FinalProject/project.py
+++ FinalProject/FinalProject/project.py
@@ -41,6 +41,7 @@ MAP = f"################" \
           f"################" \
 
 def main():
+    global t
     canvas = Canvas(1440, 900)
     canvas.set_canvas_title("Final Project")
 
@@ -50,12 +51,16 @@ def main():
     enemy = Enemy(canvas, ENEMY_WIDTH, ENEMY_HEIGHT, 100)
     player = Player(canvas, gun)
 
+
+
     while True:
         clicks = canvas.get_new_mouse_clicks()
         canvas.update()
         if len(clicks) > 0:
+            # this functions must work asynchoronously
             player.shoot()
-            enemy.check_for_bullet(player.fire_triangule)
+            enemy.take_damage(DAMAGE_AMOUNT) #this functions is temporary
+            #enemy.check_for_bullet(player.fire_triangule)
             canvas.update()
         map.generate_map()
         canvas.update()
@@ -102,31 +107,47 @@ class Player:
         self.canvas.update()
 
 class Enemy:
-    def __init__(self, canvas, width, height, health):
+    def __init__(self, canvas, width, height, max_health):
         self.canvas = canvas
-        self.health = health
+        self.max_health = max_health
         self.width = width
         self.height = height
-        self.image = canvas.create_image_with_size(canvas.get_canvas_width()/2 - width/2, canvas.get_canvas_height()/2 - height, width, height, ENEMY_SPRITE)
+        self.image = self.creeate_image(width, height)
+        self.current_health = max_health
+        self.dead = False
         canvas.update()
 
+
+    def creeate_image(self, width, height):
+        return self.canvas.create_image_with_size(self.canvas.get_canvas_width()/2 - width/2, self.canvas.get_canvas_height()/2 - height, width, height, ENEMY_SPRITE)
+
     def take_damage(self, amount):
-        self.health -= amount
-        if self.health <= 0:
-            self.die()
+        if not self.dead:
+            self.current_health -= amount
+            print(self.current_health)
+            if self.current_health <= 0:
+                self.die()
+                return
+            self.canvas.delete(self.image)
+            damage_count = (self.max_health - self.current_health)/amount
+            self.image = self.creeate_image(self.width-(30 * int(damage_count)), self.height-(30 * int(damage_count)))
+            self.canvas.update()
+
 
     def die(self):
-        self.canvas.delete(self)
+        self.canvas.delete(self.image)
         self.canvas.update()
+        self.dead = True
 
     def check_for_bullet(self, fire_triangule):
-        coords = self.canvas.coords(self.image)
+        if not self.dead:
+            coords = self.canvas.coords(self.image)
 
-        collided_list = self.canvas.find_overlapping(coords[0], coords[1], coords[0] + self.width, coords[1] + self.height)
-        for col in collided_list:
-            if col is fire_triangule:
-                self.take_damage(DAMAGE_AMOUNT)
-                print(self.health)
+            collided_list = self.canvas.find_overlapping(coords[0], coords[1], coords[0] + self.width, coords[1] + self.height)
+            for col in collided_list:
+                if col is fire_triangule:
+                    self.take_damage(DAMAGE_AMOUNT)
+                    print(self.current_health)
 
 class Gun:
     def __init__(self, canvas, width, height, ammo):
